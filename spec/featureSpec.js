f491d439a0040904913199d1f4378fa9291c96a7
Ben Galley <bengalley21@gmail.com>
2020-03-17 17:11:15 +0000

    planes can't take off when weather is stormy

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -7,6 +7,7 @@ describe('Feature Test:', function(){
   beforeEach(function(){
     plane = new Plane();
     airport = new Airport();
+
   });
 
   it('planes can be instructed to land at an airport', function(){
@@ -15,8 +16,15 @@ describe('Feature Test:', function(){
   });
 
   it('planes can be instructed to takeoff', function(){
-    plane.land(airport)
+    plane.land(airport);
     plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
+  });
+
+  it('cant take off when weather is stormy', function(){
+    plane.land(airport);
+    spyOn(airport, 'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('too stormy');
+    expect(airport.planes()).toContain(plane);
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -9,5 +9,11 @@ Airport.prototype.clearForLanding = function(plane) {
   this._hangar.push(plane);
 };
 Airport.prototype.clearForTakeOff = function(plane) {
+  if(this.isStormy()) {
+    throw new Error('too stormy');
+  }
   this._hangar = [];
+};
+Airport.prototype.isStormy = function(plane) {
+  return false;
 };
